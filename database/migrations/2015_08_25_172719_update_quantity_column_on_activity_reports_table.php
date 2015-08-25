6293275d58fa5ddf8423388399041cc73cf0ca3c
Johan Alvarez <llstarscreamll@hotmail.com>
2015-08-25 22:30:09 +0000

    arreglado el error de topes hasta de 100.5 en las cantidades de las actividades mineras reportadas

---
+++ database/migrations/2015_08_25_172719_update_quantity_column_on_activity_reports_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateQuantityColumnOnActivityReportsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('activity_reports', function(Blueprint $table)
+		{
+			$table->decimal('quantity', 6, 2)->default(0)->change();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('activity_reports', function(Blueprint $table)
+		{
+			//
+		});
+	}
+
+}
