6f1ad630d9124b2207b15789d521826dc0d07496
Johan Alvarez <llstarscreamll@hotmail.com>
2016-01-18 04:48:56 +0000

    Actualizado el tooltip del botón 'Reportar Actividad Minera'

--- resources/views/activityReports/partials/action-buttons.blade.php
+++ resources/views/activityReports/partials/action-buttons.blade.php
@@ -9,9 +9,9 @@
 							</button>
 						</div>
 						
-						<a id="create-user-link" class="btn btn-default btn-sm" href="{!! route('activityReport.create') !!}" role="button"  data-toggle="tooltip" data-placement="top" title="Registrar Labor Minera">
+						<a id="create-user-link" class="btn btn-default btn-sm" href="{!! route('activityReport.create') !!}" role="button"  data-toggle="tooltip" data-placement="top" title="Registrar Actividad Minera">
 							<span class="glyphicon glyphicon-plus"></span>
-							<span class="sr-only">Registrar Labor Minera</span>
+							<span class="sr-only">Registrar Actividad Minera</span>
 						</a>
 						
 					</small>
