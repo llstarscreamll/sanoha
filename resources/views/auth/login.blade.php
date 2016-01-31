a1dd15ff20f45ae7527da5a30046ea4fd308cb42
Johan Alvarez <llstarscreamll@hotmail.com>
2016-01-31 02:42:55 +0000

    Ajuste visual del tamaño del div del formulario de inicio de sesión

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 <div class="container-fluid">
 	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
+		<div class="col-md-6 col-md-offset-3">
 			<div class="panel panel-default">
 				<div class="panel-heading">Iniciar Sesión</div>
 				<div class="panel-body">
